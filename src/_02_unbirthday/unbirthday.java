7afcf0d51d1ad3ad22d52eb64dd6871b01addfb9
League Student <league@iMac-15.attlocal.net>
2019-07-19 14:18:42 -0700

    Create unbirthday.java

---
+++ src/_02_unbirthday/unbirthday.java
@@ -0,0 +1,9 @@
+package _02_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class unbirthday {
+public static void main(String[] args) {
+	String in=JOptionPane.showInputDialog("When is your birthday?");
+}
+}
